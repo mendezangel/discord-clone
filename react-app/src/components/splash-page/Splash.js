3d731e71edcc994b6ffd39c6ac5f96de3fac06a8
edwin carrillo <edwincarrillo9@outlook.com>
2022-04-30 16:50:12 -0400

    buttons

--- react-app/src/components/splash-page/Splash.js
+++ react-app/src/components/splash-page/Splash.js
@@ -12,13 +12,7 @@ const Splash = () => {
 
     return (
         <div>
-            <div className="splash-image">
-
-            </div>
-            <div className="scrollable-obj">
-
-            </div>
-            <div className='footer'>
+            <div className='splash-image'>
 
             </div>
         </div>
