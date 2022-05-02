0d84e5a249b0596662e252152a23741901c25622
Ali <ali.naqvi.dev@gmail.com>
2022-05-02 12:25:24 -0500

    still working on routes

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import Server, User, Channel, Message, members
+from app.models import Server, User, Channel, Message, members, db
+from app.forms import CreateServerForm
 
 server_routes = Blueprint('servers', __name__)
 
@@ -10,4 +11,20 @@ def getAllServers(user_id):
     servers = User.query.join(members).filter(members.user_id == user_id).all()
     return {'servers': [server.to_dict() for server in servers]}
 
+@server_routes.route('/new')
+@login_required
+def createServer(owner_id):
+   form = CreateServerForm()
+   if form.validate_on_submit():
+        server = Server(
+            image=form.data['image'],
+            owner_id = owner_id,
+            name=form.data['name'],
 
+        )
+        db.session.add(server)
+        db.session.commit()
+
+
+@server_routes.route('/<int:server_id>')
+@login_required
