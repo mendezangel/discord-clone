97e359f508045468e6b14b729a66e323ae1ae127
Ali <ali.naqvi.dev@gmail.com>
2022-05-03 17:07:12 -0500

    signup needs error handling

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect, Link } from 'react-router-dom';
 import { signUp } from '../../store/session';
-import './LoginForm.css'
+import './LoginForm.css';
 import image from '../../images/login-background.png'
 
 const SignUpForm = () => {
@@ -44,58 +44,87 @@ const SignUpForm = () => {
     return <Redirect to='/' />;
   }
 
+
   return (
     <div className='whole-page-div'>
       <div className='background-image-container'>
-        <img className='background-image' src={image} alt='Discord Background'/>
+        <img className='background-image' src={image} alt='Discord Background' />
       </div>
-      <form onSubmit={onSignUp} className='login-form'>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
+      <div className='login-form-container'>
+        <div className='login-form-text-container'>
+          <h1>Create an account.</h1>
         </div>
-        <div>
-          <label>User Name</label>
-          <input
-            type='text'
-            name='username'
-            onChange={updateUsername}
-            value={username}
-            className='login-form-email-input'
-          ></input>
+        <div className='login-form-form-container'>
+          <form onSubmit={onSignUp} className='login-form'>
+            {/* <div>
+              {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+              ))}
+            </div> */}
+            <div className='login-form-input-fields'>
+              <div className='form-group'>
+                <label>User Name</label>
+                <input
+                  type='text'
+                  name='username'
+                  onChange={updateUsername}
+                  value={username}
+                  className='login-form-email-input'
+                />
+                {/* error handling needed */}
+              </div>
+              <div className='form-group'>
+                <label>Email</label>
+                <input
+                  type='text'
+                  name='email'
+                  onChange={updateEmail}
+                  value={email}
+                  className='login-form-email-input'
+                ></input>
+                {errors?.password?.map(error => {
+                  return (<p className="signup-error" key={error}>{error}</p>)
+                })}
+              </div>
+              <div className='form-group'>
+                <label>Password</label>
+                <input
+                  type='password'
+                  name='password'
+                  onChange={updatePassword}
+                  value={password}
+                  className='login-form-password-input'
+                ></input>
+                {errors?.password?.map(error => {
+                  return (<p className="signup-error" key={error}>{error}</p>)
+                })}
+              </div>
+              <div className='form-group'>
+                <label>Repeat Password</label>
+                <input
+                  type='password'
+                  name='repeat_password'
+                  onChange={updateRepeatPassword}
+                  value={repeatPassword}
+                  required={true}
+                  className='login-form-password-input'
+                ></input>
+                {errors?.password?.map(error => {
+                  return (<p className="signup-error" key={error}>{error}</p>)
+                })}
+              </div>
+
+            </div>
+          </form>
         </div>
-        <div>
-          <label>Email</label>
-          <input
-            type='text'
-            name='email'
-            onChange={updateEmail}
-            value={email}
-          ></input>
+        <div className='login-form-buttons-container'>
+            <button onClick={onSignUp}>Sign Up</button>
+         
+          
         </div>
-        <div>
-          <label>Password</label>
-          <input
-            type='password'
-            name='password'
-            onChange={updatePassword}
-            value={password}
-          ></input>
-        </div>
-        <div>
-          <label>Repeat Password</label>
-          <input
-            type='password'
-            name='repeat_password'
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-          ></input>
-        </div>
-        <button type='submit'>Sign Up</button>
-      </form>
-    </div>  
+        
+      </div>
+    </div>
   );
 };
 
