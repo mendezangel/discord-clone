624316ad1e9a83dcffacc585d5ba5ab01e6f90cd
mendezangel <id.angelmendez@gmail.com>
2022-05-04 09:46:10 -0700

    added channels array to servers store

--- react-app/src/components/channelbar/ChannelBar.js
+++ react-app/src/components/channelbar/ChannelBar.js
@@ -1,12 +1,15 @@
+import { useParams } from 'react-router-dom';
 import './ChannelBar.css'
 import ProfileBar from '../profilebar/ProfileBar'
 
-const ChannelBar = ({user}) => {
-    return (
-        <div className="channel-bar">
-            <ProfileBar user={user}/>
-        </div>
-    )
+const ChannelBar = ({ user }) => {
+  const params = useParams()
+  console.log(params)
+  return (
+    <div className="channel-bar">
+      <ProfileBar user={user} />
+    </div>
+  )
 }
 
 export default ChannelBar
