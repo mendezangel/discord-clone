a0ddfeebc71ff47f17cab6a45e3105848253dba3
mendezangel <id.angelmendez@gmail.com>
2022-05-04 10:16:09 -0700

    able to successfully delete

--- react-app/src/components/channelbar/ChannelBar.js
+++ react-app/src/components/channelbar/ChannelBar.js
@@ -1,12 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import './ChannelBar.css'
 import ProfileBar from '../profilebar/ProfileBar'
+import { delServer } from '../../store/server';
 
 const ChannelBar = ({ user }) => {
-  const params = useParams()
-  console.log(params)
+  const { server_id } = useParams()
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const onDelete = () => {
+    dispatch(delServer(server_id))
+    history.push('/channels/@me')
+  }
   return (
     <div className="channel-bar">
+      {server_id !== '@me' && (
+        <button onClick={onDelete}>Delete</button>
+      )}
       <ProfileBar user={user} />
     </div>
   )
