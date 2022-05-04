765afd002d251041315ec9d45d779828c602bc18
Ali <ali.naqvi.dev@gmail.com>
2022-05-04 13:04:50 -0500

    finishing up styling on channels

--- react-app/src/components/channel/Channel.js
+++ react-app/src/components/channel/Channel.js
@@ -3,11 +3,14 @@ import './Channel.css'
 const Channel = () => {
   return (
   <div className="channel-container">
+    
     <div className='channel-name'>
+      <i class="fas fa-hashtag"></i>
       yo bro
     </div>
     <div className='channel-delete'>
-      <button>delete</button>
+    <i class="fa-solid fa-gear"></i>
+    <i class="fa fa-trash" aria-hidden="true"></i>
     </div>
   </div>
   )
--- react-app/src/components/channelbar/ChannelBar.js
+++ react-app/src/components/channelbar/ChannelBar.js
@@ -7,23 +7,31 @@ import Channel from '../channel/Channel'
 import { delServer } from '../../store/server';
 
 const ChannelBar = ({ user }) => {
-  const { server_id } = useParams()
-  const dispatch = useDispatch()
-  const history = useHistory()
+    const { server_id } = useParams()
+    const dispatch = useDispatch()
+    const history = useHistory()
 
-  const onDelete = () => {
-    dispatch(delServer(server_id))
-    history.push('/channels/@me')
-  }
-  return (
-    <div className="channel-bar">
-      {server_id !== '@me' && (
-        <button onClick={onDelete}>Delete</button>
-      )}
-      <Channel />
-      <ProfileBar user={user} />
-    </div>
-  )
+    const onDelete = () => {
+        dispatch(delServer(server_id))
+        history.push('/channels/@me')
+    }
+    return (
+        <div className="channel-bar">
+            
+            <div className="channel-bar-top">
+                {server_id !== '@me' && (
+                    <div className='channel-bar-server-info'>
+                    <button onClick={onDelete} className="server-button">Delete</button>
+                    <button className="server-button">Edit</button>
+                    </ div>
+                )}
+                <p className='channel-bar-p'>CHANNELS</p>
+                <Channel />
+            </div>
+
+            <ProfileBar user={user} />
+        </div>
+    )
 }
 
 export default ChannelBar
