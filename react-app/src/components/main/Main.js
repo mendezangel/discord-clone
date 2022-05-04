920bd0e1b736a13c0a1e5a5b5055b21c6ded2111
edwin carrillo <edwincarrillo9@outlook.com>
2022-05-04 12:45:16 -0400

    sending pr

--- react-app/src/components/main/Main.js
+++ react-app/src/components/main/Main.js
@@ -4,10 +4,14 @@ import './Main.css'
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getAllServers } from "../../store/server.js"
+import { useParams } from "react-router-dom"
+import ServerMembers from "../servermembers/ServerMembers.js"
+import ChatBox from "../chatbox/ChatBox.js"
 
 
 const Main = () => {
     const dispatch = useDispatch()
+    const {server_id} = useParams()
     const user = useSelector(state => state.session.user)
     const servers = useSelector(state => state.server.servers)
     let normalized_servers = [];
@@ -31,8 +35,10 @@ const Main = () => {
 
     return (
         <div className="main">
+
             <ServerBar servers={normalized_servers}/>
             <ChannelBar user={user}/>
+            {server_id === '@me'? null :<><ChatBox /><ServerMembers /></>}
         </div>
     )
 }
