18c1d448766b67346a2c0b11b3141cc43ebe210f
edwin carrillo <edwincarrillo9@outlook.com>
2022-05-04 16:40:09 -0400

    fixed members table stuff

--- react-app/src/components/servermembers/ServerMembers.js
+++ react-app/src/components/servermembers/ServerMembers.js
@@ -6,7 +6,10 @@ const ServerMembers = ({members}) => {
             {members?.map((member,idx) => {
                 const name = member?.username.split('#')
                 return (
-                    <div key={idx} className='member'>{name[0]}</div>
+                    <div key={idx} className='member'>
+                        <img className='members-image' src={member.profile_pic} height='32px'/>
+                        <p>{name[0]}</p>
+                    </div>
                 )
             })}
         </div>
