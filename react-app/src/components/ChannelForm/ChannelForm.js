c9e7ad2aa28b86cfe7add4c2cc34b2a908e8dc66
Ali <ali.naqvi.dev@gmail.com>
2022-05-04 15:51:34 -0500

    channel-form

---
+++ react-app/src/components/ChannelForm/ChannelForm.js
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+
+
+
+
+const ChannelForm = () => {
+  const [name, setName] = useState("")
+
+  return (
+    <div className='whole-page-div'>
+    <div className="server-form-container">
+      <div className="server-form-text-container">
+        <h1>Create a new channel.</h1>
+        
+      </div>
+    </div>  
+  )
+
+
+
+}
