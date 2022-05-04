72e1d83452df539982785b264297a0373376d4ec
mendezangel <id.angelmendez@gmail.com>
2022-05-04 10:41:40 -0700

    debugging server bar not rerendering

--- react-app/src/components/main/Main.js
+++ react-app/src/components/main/Main.js
@@ -10,38 +10,38 @@ import ChatBox from "../chatbox/ChatBox.js"
 
 
 const Main = () => {
-    const dispatch = useDispatch()
-    const {server_id} = useParams()
-    const user = useSelector(state => state.session.user)
-    const servers = useSelector(state => state.server.servers)
+  const dispatch = useDispatch()
+  const { server_id } = useParams()
+  const user = useSelector(state => state.session.user)
+  const servers = useSelector(state => state.servers)
 
-    let normalized_servers = [];
-    if (Array.isArray(servers)) {
-      for (let i = 0; i < Object.keys(servers).length; i++) {
-        normalized_servers.push(servers[Object.keys(servers)[i]])
-      }
-    }
-    const members = useSelector(state => state.server.members)
-    let normalized_members = [];
-    if (Array.isArray(members)) {
-      console.log(members.flat())
-      for (let i = 0; i < Object.keys(members).length; i++) {
-        normalized_members.push(members[Object.keys(members)[i]])
-      }
-    }
+  // let normalized_servers = [];
+  // if (Array.isArray(servers)) {
+  //   for (let i = 0; i < Object.keys(servers).length; i++) {
+  //     normalized_servers.push(servers[Object.keys(servers)[i]])
+  //   }
+  // }
+  // const members = useSelector(state => state.server.members)
+  // let normalized_members = [];
+  // if (Array.isArray(members)) {
+  //   console.log(members.flat())
+  //   for (let i = 0; i < Object.keys(members).length; i++) {
+  //     normalized_members.push(members[Object.keys(members)[i]])
+  //   }
+  // }
 
-    useEffect(() => {
-        dispatch(getAllServers(user?.id))
-    }, [dispatch, user?.id])
+  useEffect(() => {
+    dispatch(getAllServers(user?.id))
+  }, [dispatch, user?.id])
 
-    return (
-        <div className="main">
+  return (
+    <div className="main">
 
-            <ServerBar servers={normalized_servers}/>
-            <ChannelBar user={user}/>
-            {server_id === '@me'? null :<><ChatBox /><ServerMembers /></>}
-        </div>
-    )
+      <ServerBar servers={servers?.servers} />
+      <ChannelBar user={user} />
+      {server_id === '@me' ? null : <><ChatBox /><ServerMembers /></>}
+    </div>
+  )
 }
 
 export default Main
