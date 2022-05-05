1e15fcad967958d13868c8f08a5765879ea86493
tristan <thuckabee.work@gmail.com>
2022-05-05 18:12:53 -0500

    current work

--- react-app/src/components/channelbar/ChannelBar.js
+++ react-app/src/components/channelbar/ChannelBar.js
@@ -11,40 +11,47 @@ const ChannelBar = ({ user }) => {
     const dispatch = useDispatch()
     const history = useHistory()
     const server = useSelector(state => state.server[server_id]);
+    console.log(server)
     const channelState = useSelector(state => state.channel);
     const channels = channelState.channels;
 
-    const onDelete = () => {
-        dispatch(delServer(server_id))
-        history.push('/channels/@me')
-    }
-    const editButton = () => {
+    // const onDelete = () => {
+    //     dispatch(delServer(server_id))
+    //     history.push('/channels/@me')
+    // }
+    // const editButton = () => {
+    //   history.push({
+    //     pathname: `/channels/${server_id}/edit`,
+    //     state: server
+    //   })
+    // }
+    const createDM = () => {
       history.push({
-        pathname: `/channels/${server_id}/edit`,
-        state: server
+        pathname: '/channels/new',
+        server_id
       })
     }
-    const onClick = () => {
-        history.push({
-          pathname: '/channels/new',
-          server_id
-        })
-    }
+    // const onClick = () => {
+    //     history.push({
+    //       pathname: '/channels/new',
+    //       server_id
+    //     })
+    // }
 
     return (
         <div className="channel-bar">
             
             <div className="channel-bar-top">
-                <h2 className='server-name-text'>{server?.name}</h2>
+                <h2 className='server-name-text'>{user?.username}</h2>
                 {server_id !== '@me' && (
                   <>
                     <div className='channel-bar-server-info'>
-                      <button onClick={onDelete} className="server-button">Delete</button>
-                      <button className="server-button" onClick={editButton}>Edit</button>
+                      <button /*onClick={onDelete}*/ className="server-button">Delete</button>
+                      <button className="server-button" /*onClick={editButton}*/ >Edit</button>
                     </ div>
                     <div className='channel-bar-text'>
                         <p className='channel-bar-p'>CHANNELS</p>
-                        <i className="fas fa-plus" onClick={onClick}></i>
+                        <i className="fas fa-plus" onClick={createDM}></i>
                     </div>
                     { channels?.map( channel => {
                         if (channel.server_id === server?.id) {
@@ -57,6 +64,10 @@ const ChannelBar = ({ user }) => {
                       })}
                   </>
                 )}
+                <div className='channel-bar-text'>
+                    <p className='channel-bar-p'>Private Messages</p>
+                    <i className="fas fa-plus" onClick={createDM}></i>
+                </div>
             </div>
 
             <ProfileBar user={user} />
