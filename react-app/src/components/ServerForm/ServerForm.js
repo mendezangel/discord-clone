42f2c08f0463b56ae303a2619129d97484fc1fb5
Ali <ali.naqvi.dev@gmail.com>
2022-05-05 00:13:20 -0500

    styled on homepage a bit with new fonts and started styling the server/channel create forms

--- react-app/src/components/ServerForm/ServerForm.js
+++ react-app/src/components/ServerForm/ServerForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-
+import "./ServerForm.css"
 import { createServer } from '../../store/server';
 
 const ServerForm = () => {
