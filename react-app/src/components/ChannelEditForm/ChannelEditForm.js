e4f991b985663e0ec7f76814a5069344dc94d9eb
mendezangel <id.angelmendez@gmail.com>
2022-05-07 11:35:07 -0700

    styled channel edit form as well as functional validations

--- react-app/src/components/ChannelEditForm/ChannelEditForm.js
+++ react-app/src/components/ChannelEditForm/ChannelEditForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
-import { editChannel } from '../../store/channels'
+import { createChannel } from '../../store/channels'
+import '../ChannelForm/ChannelForm.css'
 
 
 const ChannelEditForm = () => {
@@ -14,16 +15,14 @@ const ChannelEditForm = () => {
   const updateName = (e) => setName(e.target.value);
   const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(location.state)
     const channel = {
-      id: location.state.id,
       name,
-      server_id: location.state.server_id
+      server_id: location.server_id
     };
-    console.log(channel)
-    const editedChannel = await dispatch(editChannel(channel));
-    if (editedChannel.errors) return setErrors(editedChannel.errors);
-    history.push(`/channels/${location.state.server_id}/${editedChannel.id}`);
+    const newChannel = await dispatch(createChannel(channel));
+
+    if (newChannel.errors) return setErrors(newChannel.errors.name);
+    history.push(`/channels/${location.server_id}/${newChannel.id}`);
   }
   const backButton = () => {
     history.goBack();
@@ -31,9 +30,9 @@ const ChannelEditForm = () => {
 
   return (
     <div className='whole-page-div'>
-      <div className="login-form-container">
+      <div className="signup-form-container">
         <div className="login-form-text-container">
-          <h1>Edit channel name.</h1>
+          <h1> Edit Channel</h1>
         </div>
         <div className="login-form-input">
           <form
@@ -44,24 +43,24 @@ const ChannelEditForm = () => {
               <label>Name</label>
               <input
                 type="text"
-                className="login-form-name-input"
+                className="input"
+                name='name'
                 value={name}
                 onChange={updateName}
               />
-              { errors?.map( error => {
-                return (<p className="login-form-error" key={error}>{error}</p>)
+              {errors?.map(error => {
+                return (<p className="signup-error" key={error}>{error}</p>)
               })}
             </div>
 
           </form>
         </div>
-        <div className='login-form-buttons-container'>
-          <button className='server-form-back-button' onClick={backButton}>Back</button>
-          <button className='server-form-create-button' onClick={onSubmit} type='submit'>Create</button>
+        <div className='create-channel-buttons-container'>
+          <button className='channel-form-button' onClick={backButton}>Back</button>
+          <button className='channel-form-button' onClick={onSubmit} type='submit'>Create</button>
         </div>
       </div>
     </div>
   )
 }
-
 export default ChannelEditForm;
