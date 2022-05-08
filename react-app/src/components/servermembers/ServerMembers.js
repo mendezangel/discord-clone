c3b2793f7276581afc11a8d3725524638e141e49
Ali <ali.naqvi.dev@gmail.com>
2022-05-08 03:51:44 -0500

    finished popup modals that show member name to the left upon hover

--- react-app/src/components/servermembers/ServerMembers.js
+++ react-app/src/components/servermembers/ServerMembers.js
@@ -1,26 +1,37 @@
 import './ServerMembers.css'
+import Popup from 'reactjs-popup'
 
-const ServerMembers = ({members}) => {
-    if(members !== undefined){
+const ServerMembers = ({ members }) => {
+    if (members !== undefined) {
         let arr = []
-        for(let member in members){
+        for (let member in members) {
             arr.push(members[member])
         }
         return (
-            <div className='members-list'>
-            {arr?.map((member,idx) => {
-                const name = member?.username.split('#')
-                return (
-                    <div key={idx} className='member'>
-                        <img className='members-image' src={member.profile_pic} height='32px' alt="user profile" />
-                        <p>{name[0]}</p>
-                    </div>
-                )
-            })}
 
-        </div>
-    )
-}
-return null
+            <div className='members-list'>
+                {arr?.map((member, idx) => {
+                    const name = member?.username.split('#')
+                    return (
+                        <Popup
+                            trigger={open => (
+                            <div key={idx} className='member'>
+                                <img className='members-image' src={member.profile_pic} height='32px' alt="user profile" />
+                                <p>{name[0]}</p>
+                            </div>
+                            )}
+                            position="left center"  
+                            closeOnEscape
+                            on={'hover'}
+                        >
+                            <p>{name}</p>
+                        </Popup>
+                    )
+                })}
+
+            </div>
+        )
+    }
+    return null
 }
 export default ServerMembers
