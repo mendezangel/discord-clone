aafdb4bab287c0ab35b7bf7db88330d6e6aa68be
Ali <ali.naqvi.dev@gmail.com>
2022-12-30 02:16:27 -0600

    render-deployment-ali

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from .config import Config
 
 from .socket import socketio
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
 # Setup login manager
 login = LoginManager(app)
